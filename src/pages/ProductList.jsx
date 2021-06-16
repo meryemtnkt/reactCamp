23a8da48fc7d472318118fc2dba5620b5dce3128
meryemtankut <meryemtankut94@gmail.com>
2021-06-17 00:09:25 +0300

    12 haziran son

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
-import { Icon, Menu, Table } from 'semantic-ui-react'
+import { Icon, Menu, Table,Button } from 'semantic-ui-react'
 import { useState,useEffect } from "react";
 import ProductService from '../services/productService';
 import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { product } from 'prelude-ls';
+import { addToCart } from '../store/actions/cartActions';
+import { toast } from 'react-toastify';
 
 export default function ProductList() {
+    const  dispatch = useDispatch()
 
     const [products, setProducts] = useState([]);
     
@@ -14,6 +19,11 @@ export default function ProductList() {
 
     },[]);
     
+    const handleAddToCart = (product)=>{
+        dispatch(addToCart(product))
+        toast.success()
+    }
+
     return (
         <div>
             <Table celled>
@@ -23,7 +33,7 @@ export default function ProductList() {
                         <Table.HeaderCell>id</Table.HeaderCell>
                         <Table.HeaderCell>title</Table.HeaderCell>
                         <Table.HeaderCell>body</Table.HeaderCell>
-
+                        <Table.HeaderCell>Kategori</Table.HeaderCell>
                     </Table.Row>
                 </Table.Header>
 
@@ -35,6 +45,7 @@ export default function ProductList() {
                                 <Table.Cell>{product.id}</Table.Cell>
                                 <Table.Cell>{product.title}</Table.Cell>
                                 <Table.Cell>{product.body}</Table.Cell>
+                                <Table.Cell><Button onClick={() => handleAddToCart(product)}>Sepete Ekle</Button></Table.Cell>
                             </Table.Row>
                         ))
                     }
