9a191d551fb949340cfab0d7d72c05f08540612c
meryemtankut <meryemtankut94@gmail.com>
2021-06-29 00:39:38 +0300

    yarn

---
+++ src/pages/productAdd.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {formik} from 'formik'
+
+
+export default function ProductAdd(){
+
+    return(
+        <div>
+            <Formik></Formik>
+
+
+        </div>
+    )
+
+}
